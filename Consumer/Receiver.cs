08c6434239d8d41a8fdc2489fab169599fd5f998
Salvation Kemuel <skemuel007@gmail.com>
2022-06-18 21:41:48 +0100

    Consumer part completed

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,38 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory()
+            {
+                HostName = "localhost"
+            };
+
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("BasicTest", false, false, false, null);
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, eventArgs) =>
+                {
+                    // var body = ea.Body;
+                    var body = eventArgs.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+
+                    Console.WriteLine("Received message: {0}...", message);
+                };
+
+                channel.BasicConsume("BasicTest", true, consumer);
+                Console.WriteLine("Press [enter] to exit the Consumer...");
+                Console.ReadLine();
+            }
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,12 @@
-# ProducerConsumerRabbitMQ
+# Producer Consumer with RabbitMQ
+Simple console application with producer and consumer side using rabbitmq factory to create connection from .net core 3.1 application, producing a message by a sender with a receiver consuming the message.
+## Technology
+.NET 3.1 Console Application, RabbitMQ and Docker
+
+## Setup
+> Ensure dotnet core 3.1 is installed on your local machine, also ensure docker desktop is installed as well
+> Run rabbitmq using docker command `docker run -d --hostname rabbitmq --name rabbit-mq -p 5672:5672 -p 9090:15672 rabbitmq:3-management`
+> Open your browser and enter the address `localhost:9090`
+> Use user name: ***guest*** and password ***guest*** to connect
+> N/B: rabbitmq exposes port ***5672*** for *amq* connection by dotnet core
+> Navigate into the project folder and Run the command `dotnet run`
